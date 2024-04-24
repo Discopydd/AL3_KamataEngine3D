e666f47f2d7b2149dc2fd5782f9a855dbde883c4
LE2C_29_リ_ヨン <136088534+Discopydd@users.noreply.github.com>
2024-04-24 13:54:21 +0900

    各機能

--- DirectXGame/scene/GameScene.cpp
+++ DirectXGame/scene/GameScene.cpp
@@ -1,11 +1,15 @@
 #include "GameScene.h"
 #include "TextureManager.h"
 #include <cassert>
-
+#include"ImGuiManager.h"
+#include"PrimitiveDrawer.h"
+#include"AxisIndicator.h"
 GameScene::GameScene() {}
 
 GameScene::~GameScene() {
 	delete sprite_;
+	delete model_;
+	delete debugCamera_;
 }
 
 void GameScene::Initialize() {
@@ -15,14 +19,47 @@ void GameScene::Initialize() {
 	audio_ = Audio::GetInstance();
 	textureHandle_ = TextureManager::Load("tex1.png");
 	sprite_ = Sprite::Create(textureHandle_, { 100,50 });
-	
+
+	//
+	model_ = Model::Create();
+	worldTransform_.Initialize();
+	viewProjection_.Initialize();
+
+	//
+	soundDataHandle_ = audio_->LoadWave("mokugyo.wav");
+	audio_->PlayWave(soundDataHandle_);
+	voiceHandle_ = audio_->PlayWave(soundDataHandle_, true);
+
+	//
+	PrimitiveDrawer::GetInstance()->SetViewProjection(&viewProjection_);
+
+	//
+	debugCamera_ = new DebugCamera(1280, 720);
+
+	//
+	AxisIndicator::GetInstance()->SetVisible(true);
+	AxisIndicator::GetInstance()->SetTargetViewProjection(&debugCamera_->GetViewProjection());
 }
 
 void GameScene::Update() {
-Vector2 position = sprite_->GetPosition();
-	position.x += 2.0f;
-	position.y += 1.0f;
-	sprite_->SetPosition(position);
+
+//Vector2 position = sprite_->GetPosition();
+//	position.x += 2.0f;
+//	position.y += 1.0f;
+//	sprite_->SetPosition(position);
+
+	/*if (input_->TriggerKey(DIK_SPACE)) {
+		audio_->StopWave(voiceHandle_);
+	}*/
+
+	//#ifdef _DEBUG
+	//ImGui::Begin("Debug1");
+	//ImGui::InputFloat3("InputFloat3", inputFloat3);
+	//ImGui::SliderFloat3("SliderFloat3", inputFloat3, 0.0f, 1.0f);
+	//ImGui::ShowDemoWindow();
+	//ImGui::End();
+	//#endif
+	debugCamera_->Update();
 }
 
 void GameScene::Draw() {
@@ -52,8 +89,16 @@ void GameScene::Draw() {
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
 
+	//model_->Draw(worldTransform_,viewProjection_,textureHandle_);
+	
+	model_->Draw(worldTransform_,debugCamera_->GetViewProjection(), textureHandle_);
+
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
+
+	/*PrimitiveDrawer::GetInstance()->DrawLine3d({ 0,0,0 }, { 0,10,0 }, { 1.0f,0.0f,0.0f,1.0f });
+	PrimitiveDrawer::GetInstance()->DrawLine3d({ 0,0,0 }, { 10,0,0 }, { 1.0f,0.0f,0.0f,1.0f });*/
+
 #pragma endregion
 
 #pragma region 前景スプライト描画
@@ -63,7 +108,8 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに前景スプライトの描画処理を追加できる
 	/// </summary>
-	sprite_->Draw();
+	//sprite_->Draw();
+	
 
 	// スプライト描画後処理
 	Sprite::PostDraw();
