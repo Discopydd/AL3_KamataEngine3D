34a0f317209fe4a94cfa0110c3708791542afa99
LE2C_29_リ_ヨン <136088534+Discopydd@users.noreply.github.com>
2024-05-30 12:40:10 +0900

    02_05完了

--- DirectXGame/Player.cpp
+++ DirectXGame/Player.cpp
@@ -1,5 +1,7 @@
 #include "Player.h"
 
+Player::~Player() { delete model_; }
+
 void Player::Initialize(Model* model,ViewProjection*viewProjection, const Vector3& position)
 {
 	assert(model);
@@ -12,6 +14,65 @@ void Player::Initialize(Model* model,ViewProjection*viewProjection, const Vector
 
 void Player::Update()
 {
+	if (Input::GetInstance()->PushKey(DIK_RIGHT) || Input::GetInstance()->PushKey(DIK_LEFT)) {
+		Vector3 acceleration{};
+		if (Input::GetInstance()->PushKey(DIK_RIGHT)) {
+			if (lrDirection_ != LRDirection::kRight) {
+				lrDirection_ = LRDirection::kRight;
+				turnStartRotationY_ = worldTransform_.rotation_.y;
+				turnUseRotationY_ = 0 - worldTransform_.rotation_.y;
+				turnNowFram_ = 1;
+			}
+			if (velocity_.x < 0) {
+				velocity_.x *= (1 - kAttenuation);
+			}
+			acceleration.x += kAcceleration;
+		} else if (Input::GetInstance()->PushKey(DIK_LEFT)) {
+			if (lrDirection_ != LRDirection::kLeft) {
+				lrDirection_ = LRDirection::kLeft;
+				turnStartRotationY_ = worldTransform_.rotation_.y;
+				turnUseRotationY_ = acosf(-1) - worldTransform_.rotation_.y;
+				turnNowFram_ = 1;
+			}
+			if (velocity_.x > 0) {
+				velocity_.x *= (1 - kAttenuation);
+			}
+			acceleration.x -= kAttenuation;
+		}
+
+		velocity_ += acceleration;
+		velocity_.x = std::clamp(velocity_.x, -kLimitRunSpeed, kLimitRunSpeed);
+	} else {
+		velocity_.x *= (1 - kAttenuation);
+	}
+	//
+	if (!isJump_) {
+		if (Input::GetInstance()->PushKey(DIK_UP)) {
+			isJump_ = true;
+			isGround_ = false;
+			velocity_ += Vector3(0, kJumpAcceleration_, 0);
+		}
+	} else {
+		velocity_ += Vector3(0, -kGravityAcceleration_, 0);
+		velocity_.y = max(velocity_.y, -kLimitFallSpeed_);
+	}
+	if (velocity_.y < 0) {
+		if (worldTransform_.translation_.y <= 2) {
+			isJump_ = false;
+			isGround_ = true;
+			worldTransform_.translation_.y = 2;
+			velocity_.y = 0;
+		}
+	}
+
+	worldTransform_.translation_ += velocity_;
+
+	if (turnNowFram_ >= 1 && turnNowFram_ < turnEndFrame_) {
+		turnNowFram_++;
+		float easing = powf(float(turnNowFram_) / float(turnEndFrame_), 3);
+		worldTransform_.rotation_.y = turnUseRotationY_ * easing + turnStartRotationY_;
+	}
+
 	worldTransform_.UpdateMatrix();
 }
 
